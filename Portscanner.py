7ee935ec14fc19cb5d74e6fcb6432f1ac9291fbe
Martin Tastler <123328309+Martin2p@users.noreply.github.com>
2025-06-17 20:10:55 +0200

    Create Portscanner.py

---
+++ Portscanner.py
@@ -0,0 +1,26 @@
+"""
+Copyright © 2025 Martin Tastler
+
+DEUTSCH:
+Dieses Programm und der Quellcode dürfen ausschließlich für private und nicht-kommerzielle Zwecke verwendet werden. 
+Jede kommerzielle Nutzung, Veränderung, Verbreitung oder Veröffentlichung ist ohne ausdrückliche schriftliche Erlaubnis des Autors untersagt.
+
+Das Kopieren oder Verwenden einzelner Codebestandteile für andere Projekte ist ebenfalls nicht gestattet, sofern keine vorherige Zustimmung vorliegt.
+Bei Interesse an einer kommerziellen Nutzung oder Lizenzierung wenden Sie sich bitte an den Autor.
+
+ENGLISH:
+This software and its source code may only be used for private and non-commercial purposes.
+Any commercial use, modification, distribution, or publication is strictly prohibited without the prior written permission of the author.
+
+Copying or using individual parts of the code in other projects is also not permitted unless approved in advance.
+
+For commercial licensing inquiries or permission requests, please contact the author.
+
+-----------------------------------------
+Author / Autor: Martin Tastler
+
+martin.tastler@posteo.de
+
+Date: June 2025
+
+"""
